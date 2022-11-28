a21dbddd2bdf57ec0f99798066f76875035bdd31
Felipe <86206681+Felpszadaa@users.noreply.github.com>
2022-11-28 20:07:27 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+var numberToFind = 0
+var attemps = 0
+
+function refresh() {
+    //GERA UM NÚMERO ALEATÓRIO
+    attemps = 0
+    numberToFind = parseInt(Math.random() * 100 )
+}
+
+refresh()
+
+function verifyNumber() {
+    var bet = document.getElementById('bet').value
+    if (bet > 100 || bet < 1) {
+        alert('Aposta inválida')
+        return
+    }
+
+    if (bet > numberToFind) {
+        attemps++
+        alert('O número para ser encontrado é MENOR')
+    } else if (bet < numberToFind) {
+        attemps++
+        alert('O número para ser encontrado é MAIOR')
+    } else {
+        alert('Parabéns você acertou! Com ' + attemps + ' erros')
+        refresh()
+    }
+}
